e8c8a7af081b5ed614375fb68ffe4772fb51d71c
ritwikbera <ritwikbera12@gmail.com>
2020-04-03 19:59:10 -0500

    fixed memory logging using hooks

--- checkpointing.py
+++ checkpointing.py
@@ -11,7 +11,10 @@ def _get_gpu_mem(synchronize=True, empty_cache=True):
 
 def _generate_mem_hook(handle_ref, mem, idx, hook_type, exp):
     def hook(self, *args):
-        if len(mem) == 0 or mem[-1]["exp"] != exp:
+        
+        print('hook called')
+
+        if len(mem) == 0:
             call_idx = 0
         else:
             call_idx = mem[-1]["call_idx"] + 1
@@ -28,6 +31,9 @@ def _generate_mem_hook(handle_ref, mem, idx, hook_type, exp):
             'mem_cached': mem_cached,
         })
 
+        if exp=='checkpointed':
+            print(m[-1])
+
     return hook
 
 
@@ -41,33 +47,20 @@ def _add_memory_hooks(idx, mod, mem_log, exp, hr):
     h = mod.register_backward_hook(_generate_mem_hook(hr, mem_log, idx, 'bwd', exp))
     hr.append(h)
 
+    print('Hooks added for {}'.format(exp))
 
-def log_mem(model, inp, mem_log=None, exp=None):
-    mem_log = mem_log or []
-    exp = exp or f'exp_{len(mem_log)}'
+
+def log_mem(model, inp, exp, cp_chunks=1):
+    mem_log = []
     hr = []
     for idx, module in enumerate(model.modules()):
         _add_memory_hooks(idx, module, mem_log, exp, hr)
 
     try:
-        out = model(inp)
-        loss = out.sum()
-        loss.backward()
-    finally:
-        [h.remove() for h in hr]
-
-        return mem_log
-
-
-def log_mem_cp(model, inp, mem_log=None, exp=None, cp_chunks=3):
-    mem_log = mem_log or []
-    exp = exp or f'exp_{len(mem_log)}'
-    hr = []
-    for idx, module in enumerate(model.modules()):
-        _add_memory_hooks(idx, module, mem_log, exp, hr)
-
-    try:
-        out = checkpoint_sequential(model, cp_chunks, inp)
+        if cp_chunks > 1:
+            out = checkpoint_sequential(model, cp_chunks, inp)
+        else:
+            out = model(inp)
         loss = out.sum()
         loss.backward()
     finally:
@@ -83,18 +76,23 @@ def plot_mem(df, exps=None, normalize_call_idx=True, normalize_mem_all=True):
     fig, ax = plt.subplots(figsize=(20, 10))
     
     for exp in exps:
-        df_ = df[df.exp == exp]
+        try:
+            df_ = df[df.exp == exp]
+        except AttributeError:
+            continue
+
+        print(df_)
 
         if normalize_call_idx:
             df_.call_idx = df_.call_idx / df_.call_idx.max()
 
         if normalize_mem_all:
-            df_.mem_all = df_.mem_all - df_[df_.call_idx == df_.call_idx.min()].mem_all
+            df_.mem_all = df_.mem_all - df_[df_.call_idx == df_.call_idx.min()].mem_all.iloc[0]
             df_.mem_all = df_.mem_all // 2 ** 20
 
         plot = df_.plot(ax=ax, x='call_idx', y='mem_all', label=exp)
 
-        plot.get_figure().savefig('{} checkpointed'.format(exp))
+        plot.get_figure().savefig('{}'.format(exp))
 
 
 if __name__=='__main__':
@@ -103,13 +101,8 @@ if __name__=='__main__':
     input = torch.rand(bs, 3, 224, 224).cuda()
     mem_log = []
 
-    try:
-        mem_log.extend(log_mem(model, input, exp='baseline'))
-        mem_log.extend(log_mem_cp(model, input, exp='checkpointed'))
-
-    except Exception as e:
-        print(f'log_mem failed because of {e}')
+    mem_log.extend(log_mem(model, input, exp='baseline'))
+    mem_log.extend(log_mem(model, input, exp='checkpointed', cp_chunks=3))
 
     df = pd.DataFrame(mem_log)
-
     plot_mem(df, exps=['baseline','checkpointed'])
